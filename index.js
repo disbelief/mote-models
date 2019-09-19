7d9f573ba333a9c2ae769b3ed6561adc7ba4953e
Michael Lawlor <mike@auka.ca>
2019-09-19 17:14:20 +0100

    Make the default package entrypoint compiled version

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-import Model from './src/Model';
-import Attribute from './src/Attribute';
-import User from './src/User';
+"use strict";
 
-export default { Model, Attribute, User };
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _Model = _interopRequireDefault(require("./src/Model"));
+
+var _Attribute = _interopRequireDefault(require("./src/Attribute"));
+
+var _User = _interopRequireDefault(require("./src/User"));
+
+var _default = {
+  Model: _Model.default,
+  Attribute: _Attribute.default,
+  User: _User.default
+};
+exports.default = _default;
